6e8c806429914abe8a1cb704016cfad15d79fccf
tomoda <salt.626kg@gmail.com>
2026-07-10 17:15:03 +0900

    Update project settings

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,28 @@
-export default function Home() {
+import { client } from "@/lib/microcms";
+
+function Header() {
+  return <h1>会社名</h1>;
+}
+
+export default async function Home() {
+  const data = await client.get({
+    endpoint: "news",
+  });
+
   return (
-    <main>
-      <h1>株式会社〇〇</h1>
-      <p>コーポレートサイト制作中</p>
+    <main className="hero">
+      <Header />
+
+      <p>トップページ</p>
+
+      <h2>お知らせ</h2>
+
+      {data.contents.map((item: any) => (
+        <article key={item.id}>
+          <h3>{item.title}</h3>
+          <p>{item.content}</p>
+        </article>
+      ))}
     </main>
   );
 }
